4703c80397d7a4c3b97ec75b10f24113a016060d
grigorii <grigoriiborodin@yandex.ru>
2022-06-16 22:53:46 +0300

    add shouldGetSportsList

---
+++ src/test/java/com/lamantinov/sportwiki/requests/RequestToTheSportsDBTest.java
@@ -0,0 +1,12 @@
+package com.lamantinov.sportwiki.requests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestToTheSportsDBTest {
+
+    @Test
+    void shouldGetSportsList() {
+    }
+}
